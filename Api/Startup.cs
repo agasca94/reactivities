142696b6dbd08c7df12115a7dcfdfcc0875a112d
Arturo Gasca <agasca1994@gmail.com>
2020-07-08 17:59:37 -0500

    Add frontend client

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -30,6 +30,16 @@ namespace Api
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -46,6 +56,8 @@ namespace Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
